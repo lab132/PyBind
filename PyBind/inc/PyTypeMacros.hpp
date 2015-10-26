0a6d903c790dab996996035eb1b29b934b29166f
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-10-26 07:08:06 +0100

    Adds common types for python binding

--- PyBind/inc/PyTypeMacros.hpp
+++ PyBind/inc/PyTypeMacros.hpp
@@ -4,4 +4,5 @@
 
 #define PY_BIND_FUNCTION_NAMED( function, name ) pyb::Bind(&function).Bind<&function>(name)
 #define PY_BIND_FUNCTION( function ) PY_BIND_FUNCTION_NAMED( function, #function)
+#define PY_BIND_METHOD( class, member) PY_BIND_FUNCTION_NAMED( class::member, #member)
 #define PY_BIND_CTOR( class, ...) pyb::CtorHelper< class >::Bind< __VA_ARGS__ >()
