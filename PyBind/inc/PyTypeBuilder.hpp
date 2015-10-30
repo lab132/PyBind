be73e5b79458a96ae83db32d2e784f2a7aace728
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-10-30 09:33:06 +0100

    Adds generic pyb::BuildValue to generate arbitrary python objects

--- PyBind/inc/PyTypeBuilder.hpp
+++ PyBind/inc/PyTypeBuilder.hpp
@@ -19,6 +19,7 @@ namespace pyb
     const char* Name;
   };
 
+
   template<typename ... ArgT>
   struct ArgumentStringHelper;
 
@@ -94,6 +95,15 @@ namespace pyb
   {
     return ArgumentStringHelper<ArgT...>::BuildVerboseString();
 
+  }
+
+
+  template< typename ... ArgT>
+  Object BuildValue(ArgT... arguments)
+  {
+    static std::string argumentString = BuildFunctionArgumentString<ArgT...>();
+
+    return Object::FromNewRef(Py_BuildValue(argumentString.c_str(), arguments...));
   }
 
   template<typename T>
