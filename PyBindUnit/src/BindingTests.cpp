3e5d5df526e525ed2b024a34258c8b73fb46025d
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-11-06 08:07:30 +0000

    Adds test for Binding simple functions

--- PyBindUnit/src/BindingTests.cpp
+++ PyBindUnit/src/BindingTests.cpp
@@ -4,7 +4,10 @@
 
 bool calledFunction = false;
 
-void TestFunction1();
+void TestFunction1()
+{
+  calledFunction = true;
+}
 
 SCENARIO("Module bind Test", "[binding][module]")
 {
@@ -24,6 +27,54 @@ SCENARIO("Module bind Test", "[binding][module]")
         auto result = interpreter.RunString("import testModule");
 
         REQUIRE(result.IsValid());
+      }
+    }
+
+    interpreter.Finalize();
+  }
+}
+
+SCENARIO("Function bind Test", "[binding][function]")
+{
+  GIVEN("An interpreter with a bound module")
+  {
+    Interpreter interpreter;
+    interpreter.Initialize();
+    {
+      Module module = Module("testModule");
+
+      interpreter.RegisterModule(&module);
+
+      calledFunction = false;
+
+      WHEN("Binding a function to the module")
+      {
+        REQUIRE(calledFunction == false);
+        module.AddFunction(PY_BIND_FUNCTION(TestFunction1));
+        REQUIRE(calledFunction == false);
+
+        THEN("It should be callable")
+        {
+
+          auto result = interpreter.RunString("import testModule\ntestModule.TestFunction1()");
+          REQUIRE(calledFunction == true);
+          REQUIRE(result.IsValid());
+        }
+      }
+
+      WHEN("Binding a function to the module with a custom name")
+      {
+        REQUIRE(calledFunction == false);
+        module.AddFunction(PY_BIND_FUNCTION_NAMED(TestFunction1, "myNamedFunction"));
+        REQUIRE(calledFunction == false);
+
+        THEN("It should be callable")
+        {
+
+          auto result = interpreter.RunString("import testModule\ntestModule.myNamedFunction()");
+          REQUIRE(calledFunction == true);
+          REQUIRE(result.IsValid());
+        }
       }
     }
 
