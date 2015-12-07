c08a953483ffc7c9235377d3cd3f409900177e06
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-12-07 19:02:30 +0100

    Adds unit test for accessing list via GetItem<T>()

--- PyBindUnit/src/ListTest.cpp
+++ PyBindUnit/src/ListTest.cpp
@@ -20,3 +20,32 @@ SCENARIO("List creation", "[binding][list][create]")
     }
   }
 }
+
+SCENARIO("List access", "[binding][list][access]")
+{
+  Interpreter interpreter;
+  interpreter.Initialize();
+  GIVEN("A list created in python")
+  {
+    Object obj = interpreter.EvalString("[1,2,3,'hello']");
+    REQUIRE(obj.IsValid());
+    REQUIRE(obj.IsList());
+    List list = obj.ToList();
+    WHEN("Accessing the list")
+    {
+      REQUIRE(list.Size() == 4);
+      int val1 = list.GetItem<int>(0);
+      int val2 = list.GetItem<int>(1);
+      int val3 = list.GetItem<int>(2);
+      std::string val4 = list.GetItem<std::string>(3);
+
+      THEN("The values should have the expected value")
+      {
+        REQUIRE(val1 == 1);
+        REQUIRE(val2 == 2);
+        REQUIRE(val3 == 3);
+        REQUIRE(val4 == "hello");
+      }
+    }
+  }
+}
