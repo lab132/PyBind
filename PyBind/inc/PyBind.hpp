e97bf9f538348c601131e83637355f9452105d39
Marvin Pohl <marvin.pohl@mindyourbyte.de>
2015-12-07 07:18:24 +0100

    Adds include to List into main include file.

--- PyBind/inc/PyBind.hpp
+++ PyBind/inc/PyBind.hpp
@@ -3,6 +3,7 @@
 #include "PyBindCommon.hpp"
 #include "PyObject.hpp"
 #include "PyDictionary.hpp"
+#include "PyList.hpp"
 #include "PyInterpreter.hpp"
 #include "PyModule.hpp"
 #include "PyTypeBuilder.hpp"
@@ -11,6 +12,7 @@
 
 #include "PyObject.inl"
 #include "PyDictionary.inl"
+#include "PyList.inl"
 #include "PyInterpreter.inl"
 #include "PyModule.inl"
 #include "PyTypeObject.inl"
